50cdbbdd3650864f9110cd3bcd0a617fdf9d5e3a
sarahsturmer <93602622+sarahsturmer@users.noreply.github.com>
2022-08-10 17:01:20 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,8 @@
-
+ 
 import streamlit
 streamlit.title("My parents' new healthy diner")
-streamlit.header("Breakfast menu")
-streamlit.text("Omega-3 & blueberry oatmeal")
-streamlit.text("Kale, spinach, and rocket smoothie")
-streamlit.text("Hard-boiled free-range egg")
+streamlit.header("Breakfast Favorites")
+streamlit.text("🥣  Omega-3 & blueberry oatmeal")
+streamlit.text("🥗 Kale, spinach, and rocket smoothie")
+streamlit.text("🐔 Hard-boiled free-range egg")
+streamlit.text("🥑🍞 Avocado toast")
